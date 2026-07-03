a7c23b1fc28732a6c0641e487c09832e83e65d9a
nehajain912 <nehaj3853@gmail.com>
2026-07-03 10:23:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)   // we are using nodejs , not browser
+
+console.log(3+3);     //code readability should be high
+console.log("neha");
+
+let name="neha"
+let age=19
+let isLoggedIn=false
+
+// number 
+//bigInt -> when number is too long
+// string->single/double quote
+//bool->true/false
+//null -> standalone value
+//undefined 
+//symbol -> uniqueness
+
+
+// object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null)         //object
