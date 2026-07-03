d6ddf576b6d002bc0fbf5da71a645f2f00ce93d7
nehajain912 <nehaj3853@gmail.com>
2026-07-03 13:23:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//let score="33abc";
+//let empty=null;
+// let empty1=Number(empty);
+// console.log(typeof empty1);        // number 
+// console.log(empty1);               // 0
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+
+// let not_define;
+// let x=Number(not_define);
+// console.log(typeof x);        // number 
+// console.log(x);              // NaN
+
+// "33" -> 33
+// "33abc" -> NaN
+// null -> 0
+// true -> 1   & false -> 0
+// undefined -> undefined
+
+// let isLoggedIn="";                         
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1-> true 0-> false 
+// "" -> false
+// "neha" -> true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
