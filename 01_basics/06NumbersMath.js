7f833fa9ace7ee79247cff43c818cc3978328d34
nehajain912 <nehaj3853@gmail.com>
2026-07-04 14:41:01 +0000

    Numbers

---
+++ 01_basics/06NumbersMath.js
@@ -0,0 +1,14 @@
+const score=400
+const balance = new Number(100)
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=23.8966;
+console.log(otherNumber.toPrecision(3));
+const hundred=1000000
+console.log(hundred.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++//
