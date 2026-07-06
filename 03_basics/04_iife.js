24116abfb78dbecdbc0a2b7315685bc2081d477a
nehajain912 <nehaj3853@gmail.com>
2026-07-06 11:18:55 +0000

    immediately invoked function expression

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,25 @@
+// Immediately Invoked Function Expression (IIFE)
+/*
+global scope k pollution se problem hoti hai to prevent from that we use this
+*/
+
+(function chai() {
+    // named iife
+    console.log(`DB CONNECTED`);
+})();                                  // explicit semi colon
+
+// () -> function definition
+// () -> function execution
+
+// ( () => {
+//     console.log(`DB CONNECTED2`)
+// }  )()
+
+((name) =>
+{
+    console.log(`DB CONNECTED TWO ${name}`)
+
+})("neha")
+
+
+//  2 iife ek sth like hue hai to end m semi colon lgana first wale pr 
