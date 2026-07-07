5190a4b7f6976e80648bf635909f854700cbffb2
nehajain912 <nehaj3853@gmail.com>
2026-07-07 10:42:56 +0000

    object loop

---
+++ 05_iterators/objectLoop.js
@@ -0,0 +1,20 @@
+const myObj = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObj) {
+    //console.log(myObj[key]);
+    
+   
+}
+
+const programming = ["js","rb","C++","py"]
+for (const val in programming) {
+    
+    console.log(programming[val]);
+    
+    
+}
