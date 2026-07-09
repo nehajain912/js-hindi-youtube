15403386a56decaf67e3aa938dc0e4ebe6d07397
nehajain912 <nehaj3853@gmail.com>
2026-07-09 14:37:17 +0000

    project2-BMI Calculator

---
+++ projects/BMI calculator/script.js
@@ -0,0 +1,34 @@
+const form=document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+    if(height=='' || height<0 || isNaN(height))
+    {
+        results.innerHTML="Please give a valid height"
+    }
+    else if(weight=='' || weight<0 || isNaN(weight))
+    {
+        results.innerHTML="Please give a valid weight"
+    }
+    else
+    {
+        const BMI=(weight/((height*height)/10000)).toFixed(2)
+        let message=""
+        if(BMI<18.6)
+        {
+            message="Under Weight";
+        }
+        else if(BMI>24.9)
+        {
+            message="Over Weight";
+        }
+        else
+        {
+            message="Normal Range";
+        }
+        results.innerHTML=`<span>${BMI}</span><br>
+                          <strong>${message}</strong>`;
+    }
+})
