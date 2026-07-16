0e46b67f8e9b23af09e6c32a587386fe628502c5
nehajain912 <nehaj3853@gmail.com>
2026-07-16 12:10:05 +0000

    promises

--- 08_advance/promises.js
+++ 08_advance/promises.js
@@ -38,20 +38,20 @@
 // })
 
 // cath error
-// const promiseFour = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error = false;
-//         if(!error)
-//         {
-//             resolve({username:'Neha',password:'123'});
-//         }
-//         else{
-//             reject('ERROR Something Went Wrong');
-//         }
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error)
+        {
+            resolve({username:'Neha',password:'123'});
+        }
+        else{
+            reject('ERROR Something Went Wrong');
+        }
         
 
-//     },1000)
-// })
+    },1000)
+})
 
 
 // promiseFour.then(function(user){
@@ -63,12 +63,68 @@
 //     console.log("Something went wrong");
 // })
 
-// promiseFour.then((user) => {
-//     console.log(user);
-//     return user.username;
-// }).then( (username) => {
-//     console.log(username);
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then( (username) => {
+    console.log(username);
 
-// } ).catch(function(error){
-//     console.log(error);
-// })
+} ).catch(function(error){
+    console.log(error);
+}).finally(function(){
+    console.log("The promise is either resolved or rejected!");
+})
+
+const promiseFive = new Promise(function(resolve,reject){
+      setTimeout(function(){
+        let error = true;
+        if(!error)
+        {
+            resolve({username:'javascript',password:'123'});
+        }
+        else
+        {
+            reject('ERROR: JS went wrong')
+        }
+      },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive;
+        console.log(response);
+    } catch(error)
+    {
+        console.log(error);
+    }
+    
+}
+consumePromiseFive();
+
+// async function getAllUsers(){
+//     try{
+//     const response =await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json();
+//     console.log(data);
+  
+//     } catch(error)
+//     {
+//         console.log('E:',error);
+//     }
+    
+// }
+// getAllUsers();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch(function(error)
+{
+    console.log(error);
+}).finally(function(){
+    console.log('Data is fetched or rejected');
+})
